368f1dd696cb831b7e605e01f0986d7811e95f15
Donkasem55 <92718237+Donkasem55@users.noreply.github.com>
2026-08-14 00:24:52 +0800

    Preparedness

---
+++ ale_linux/logo/arch_linux.py
@@ -0,0 +1,14 @@
+padding = 16
+space = (" ", "white", None)
+space2 = ("   ", "white", None)
+
+logo = [
+	[(r"      /\       ", "blue", None), space],
+	[(r"     /  \      ", "blue", None), space],
+	[(r"    /    \     ", "blue", None), space],
+	[(r"   /      \    ", "blue", None), space],
+	[(r"  /   ..   \   ", "blue", None), space],
+	[(r" /   |  |   \  ", "blue", None), space],
+	[(r"/_-``    ``-_\ ", "blue", None), space]
+]
+
